9b71ef58abfa4fc3d08425b6e90545453cbc7a1c
kmaccoy <80644013+kmaccoy@users.noreply.github.com>
2021-06-02 21:57:16 -0400

    added routes

---
+++ landing-page/TokenFile 2.js
@@ -0,0 +1,7 @@
+//[!] DO NOT CODE IN THIS FILE (please)[!]
+
+//not used for now. just to make sure folders do not squish
+//and look weird
+
+//for package.json:
+// "cross-env electron ./electron"
